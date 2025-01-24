a18eb9fe88a6f6965c942ecb469e3bb664436fda
kikuchihajime0624 <kikuchi.hajime.184@gmail.com>
2025-01-24 15:55:51 +0900

    first commit

---
+++ sql/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE training_event
+(
+    event_id   varchar(10) PRIMARY KEY,
+    event_name varchar(10) NOT NULL,
+);
+
+CREATE TABLE training_parts
+(
+    parts_id   varchar(10) PRIMARY KEY ,
+    parts_name varchar(10) NOT NULL,
+)
+
+CREATE TABLE users
+(
+    user_id   INT PRIMARY KEY,
+    user_name TEXT NOT NULL
+)
+
+CREATE TABLE training_set
+(
+    training_set_id varchar(10) NOT NULL,
+    event_id        varchar(10) REFERENCES training_event (event_id),
+    parts_id        varchar(10) REFERENCES training_parts (parts_id),
+    weight          INT         NOT NULL,
+    times           INT         NOT NULL,
+    workout_date    DATE        NOT NULL,
+    user_id         varchar(10) REFERENCES users (user_id),
+    PRIMARY KEY (trainingset_id)
+);
