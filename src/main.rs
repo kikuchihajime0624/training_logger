7d02d252d11da3d10c2215332dbf9355edf05e1e
kikuchihajime0624 <kikuchi.hajime.184@gmail.com>
2025-01-27 18:17:03 +0900

    training_logger.htmlの更新、new.htmlを新規作成

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use actix_web::{App, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer};
+use askama::Template;
+use askama_actix::TemplateToResponse;
+use sqlx::{PgPool, Postgres};
+
+#[derive(Template)]
+#[template(path = "training_logger.html")]
+struct LoggerTemplate {}
+#[get("/")]
+async fn dates(pool: web::Data<Postgres>) -> HttpResponse {
+    let dates = LoggerTemplate {};
+    dates.to_response()
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()).bind(("127.0.0.1", 8080))?.run().await
+    let pool = PgPool::connect("postgres::memory:").await.unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .service(dates)
+            .app_data(web::Data::new(pool.clone()))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
-
-
-
