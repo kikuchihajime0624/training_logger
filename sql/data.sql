03e78785611735a506de876f0e924214be9f689e
kikuchihajime0624 <kikuchi.hajime.184@gmail.com>
2025-01-30 15:15:01 +0900

    setup.sql:　DATEBASE名をlogger_sample_dbに変更
    detail.tera:dbの列名に沿った引数に変更。extends,blockを追加
    
    data.sql:サンプルデータを挿入する内容に変更

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,10 @@
-INSERT INTO training_event (event_name) VALUES (exsample_event);
+INSERT INTO training_event (event_name) VALUES ('ベンチプレス');
 
-INSERT INTO training_parts (parts_name) VALUES (exsample_parts);
+INSERT INTO training_parts (parts_name) VALUES ('胸');
+
+INSERT INTO  training_set ( weight, times, workout_date ) VALUES  (100 , 10 ,'2025-01-30');
+
+
+
+
 
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,2 @@
 CREATE USER sample_user WITH PASSWORD 'sample_pass';
-CREATE DATABASE sample_db WITH OWNER sample_user;
+CREATE DATABASE logger_sample_db WITH OWNER sample_user;
