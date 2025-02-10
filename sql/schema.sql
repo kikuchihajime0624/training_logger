bf7e495c37fac908441ec57580901fb52e1fb02e
kikuchihajime0624 <kikuchi.hajime.184@gmail.com>
2025-02-10 11:10:19 +0900

    ログイン画面、ユーザー登録用のteraファイルを追加、usersテーブルの列を変更

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,8 +15,8 @@ CREATE TABLE training_parts
 
 CREATE TABLE users
 (
-    user_id   SERIAL PRIMARY KEY,
-    user_name TEXT NOT NULL UNIQUE
+    username varchar(32) PRIMARY KEY,
+    password varchar(32) NOT NULL
 );
 
 CREATE TABLE training_set
@@ -24,10 +24,10 @@ CREATE TABLE training_set
     training_set_id SERIAL NOT NULL,
     event_id        INT REFERENCES training_event (event_id),
     parts_id        INT REFERENCES training_parts (parts_id),
-    weight          INT         NOT NULL,
-    times           INT         NOT NULL,
-    workout_date    DATE        NOT NULL,
-    user_id         INT REFERENCES users (user_id),
+    weight          INT    NOT NULL,
+    times           INT    NOT NULL,
+    workout_date    DATE   NOT NULL,
+    username        varchar(32) REFERENCES users (username),
     PRIMARY KEY (training_set_id)
 );
 
